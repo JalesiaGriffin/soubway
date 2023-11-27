18296c552f250154875af271d145f74d0b9c6079
JalesiaGriffin <jgriffin@appdev.yearup.org>
2023-11-27 10:27:26 -0500

    Created getPrice method

---
+++ src/main/java/com/pluralsight/deli/PremiumTopping.java
@@ -0,0 +1,42 @@
+package com.pluralsight.deli;
+
+public class PremiumTopping extends Topping {
+    enum meats {steak, ham, salami, roastbeef, chicken, bacon};
+    enum cheeses {american, provolone, cheddar, swiss};
+    public double getPrice(String size){
+        double price = 0;
+
+        if (isMeat()){
+            switch(size){
+                case "4"-> price = 1.00;
+                case "8" -> price = 2.00;
+                case "12" -> price = 3.00;
+                default -> System.out.println("invalid.");
+            }
+        } else if (isCheese()){
+            switch(size){
+                case "4"-> price = .75;
+                case "8" -> price = 1.50;
+                case "12" -> price = 2.25;
+                default -> System.out.println("invalid.");
+            }
+        }
+    return price;
+    }
+
+    private static boolean isMeat(){
+        try (meats.valueOf(getName().toLowerCase())){
+            return true;
+        } catch (IllegalArgumentException e){
+            return false;
+        }
+    }
+
+    private static boolean isCheese(){
+        try (cheeses.valueOf(getName().toLowerCase)){
+            return true;
+        } catch (IllegalArgumentException e){
+            return false;
+        }
+    }
+}
