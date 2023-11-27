71bd595c4c5f23367ff3ddcba952e4f1d9db63b4
Jalesia Griffin <146871018+JalesiaGriffin@users.noreply.github.com>
2023-11-27 15:41:52 -0500

    updated checkout feature (#7)

--- src/main/java/com/pluralsight/deli/Order.java
+++ src/main/java/com/pluralsight/deli/Order.java
@@ -18,6 +18,29 @@ public class Order {
     }
 
     public void checkOut(){
+        System.out.println("Sandwiches");
+        sandwiches.forEach(s -> {
+            System.out.println("\n" + s.getBread() + " Sandwich");
+            System.out.println("\nToppings");
+            s.getToppings().forEach(t -> System.out.println("- "+ t));
+        });
 
+        System.out.println("\nDrinks");
+        drinks.forEach(d ->{
+            System.out.println("-" + d.getSize() + " " + d.getFlavor());
+        });
+
+        System.out.println("\nChips");
+        drinks.forEach(c ->{
+            System.out.println("-" + c.getSize() + " " + c.getFlavor());
+        });
+
+        double sandwichesTotal = sandwiches.stream().mapToDouble(Sandwich::getPrice).sum();
+        double drinksTotal = drinks.stream().mapToDouble(Drink::getPrice).sum();
+        double chipsTotal = chips.stream().mapToDouble(Chip::getPrice).sum();
+        double totalPrice = sandwichesTotal + drinksTotal + chipsTotal;
+
+        System.out.println("\n\nTotal Price");
+        System.out.println(totalPrice);
     }
 }
