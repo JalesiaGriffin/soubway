2764c4ea1454a75ddbec0e35187ceb4f6d672c30
swilson813 <swilson@appdev.yearup.org>
2023-11-27 15:59:29 -0500

    updated constructors and getPrice method

--- src/main/java/com/pluralsight/deli/Topping.java
+++ src/main/java/com/pluralsight/deli/Topping.java
@@ -4,11 +4,23 @@ public abstract class Topping implements Price {
 private String name;
 private double price;
 
-
-
-
-    @Override
-    public double getPrice() {
-        return 0;
+    public Topping(String name, double price) {
+        this.name = name;
+        this.price = price;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public abstract double getPrice();
+
 }
