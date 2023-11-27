9555ba7337e06b9afec0d6645c9de50509d5595b
swilson813 <146871062+swilson813@users.noreply.github.com>
2023-11-27 10:07:08 -0500

    Updated drinks class (#1)

--- src/main/java/com/pluralsight/deli/Drinks.java
+++ src/main/java/com/pluralsight/deli/Drinks.java
@@ -1,8 +1,47 @@
 package com.pluralsight.deli;
 
-public class Drinks implements Price{
+public class Drinks implements Price {
+    private String size;
+    private String flavor;
+
+    public Drinks(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+
+    public String getSize() {
+        return size;
+    }
+
+    public void setSize(String size) {
+        this.size = size;
+    }
+
+    public String getFlavor() {
+        return flavor;
+    }
+
+    public void setFlavor(String flavor) {
+        this.flavor = flavor;
+    }
+
+    @Override
+    public String toString() {
+        return "Drinks{" +
+                "size='" + size + '\'' +
+                ", flavor='" + flavor + '\'' +
+                '}';
+    }
+
     @Override
     public double getPrice() {
         return 0;
     }
+
+    @Override
+    public void setPrice(double Price) {
+        
+    }
+
+
 }
