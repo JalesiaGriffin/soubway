27ca384c4c0eab25f5a2873ad5e4669c4916d899
swilson813 <146871062+swilson813@users.noreply.github.com>
2023-11-28 08:45:12 -0500

    added date & time formats (#12)

--- src/main/java/com/pluralsight/deli/ReceiptFileManager.java
+++ src/main/java/com/pluralsight/deli/ReceiptFileManager.java
@@ -3,8 +3,16 @@ package com.pluralsight.deli;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class ReceiptFileManager {
+    public static void main(String[] args) {
+            ReceiptFileManager receiptFileManager = new ReceiptFileManager("receipt.txt");
+            Order order = new Order();//add parameters for order
+            receiptFileManager.saveReceiptFile(order);
+        }
+
 
     private String fileName;
 
@@ -17,8 +25,14 @@ public class ReceiptFileManager {
             //get receipt details from the order object
             String receiptDetails = order.checkOut();
 
-            //write receipt details to file
+            LocalDateTime currentDateTime = LocalDateTime.now();
+            DateTimeFormatter fm = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+            String formattedDateTime = currentDateTime.format(fm);
+
+            //write date, time & receipt details to file
+            writer.write("Date and Time: " + formattedDateTime + "\n" );
             writer.write(receiptDetails);
+
             System.out.println("Receipt file saved successfully. ");
         }catch (IOException e) {
             System.out.println("Error saving receipt file " + e.getMessage());
