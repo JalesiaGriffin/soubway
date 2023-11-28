fff93bca567b20a7ac7fe90d9b0b355e53f82ce3
swilson813 <swilson@appdev.yearup.org>
2023-11-27 23:38:58 -0500

    created saveReceiptFile method & inputted try/catch statement

--- src/main/java/com/pluralsight/deli/ReceiptFileManager.java
+++ src/main/java/com/pluralsight/deli/ReceiptFileManager.java
@@ -1,10 +1,24 @@
 package com.pluralsight.deli;
 
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
 public class ReceiptFileManager {
+
     private String fileName;
 
     public ReceiptFileManager (String fileName) {
         this.fileName = fileName;
+    }
+
+    public  saveReceiptFile (Order order) {
+        try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            //get receipt details from the order object
+            String receiptDetails = order.???();
+
+            //write receipt details to file
+            writer.write(receiptDetails);
+        }
     }
 
 
