9b230e8806fbba7cf73b923da01e0253fd97d333
swilson813 <146871062+swilson813@users.noreply.github.com>
2023-11-29 08:06:46 -0500

    made filemanager class save as a datetime.txt file (#18)

--- src/main/java/com/pluralsight/deli/ReceiptFileManager.java
+++ src/main/java/com/pluralsight/deli/ReceiptFileManager.java
@@ -7,31 +7,29 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class ReceiptFileManager {
-    public static void main(String[] args) {
-            ReceiptFileManager receiptFileManager = new ReceiptFileManager("receipt.txt");
-            Order order = new Order();//add parameters for order
-            receiptFileManager.saveReceiptFile(order);
-        }
 
+/*ReceiptFileManager receiptFileManager= new ReceiptFileManager();
+        Order order= new Order();
+        receiptFileManager.saveReceiptFile(order);*/
 
-    private String fileName;
-
-    public ReceiptFileManager (String fileName) {
-        this.fileName = fileName;
-    }
 
     public void saveReceiptFile (Order order) {
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        DateTimeFormatter fm = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss");
+        String formattedDateTime = currentDateTime.format(fm);
+
+        String fileName = formattedDateTime + ".txt";
         try(BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
             //get receipt details from the order object
            // String receiptDetails = order.checkOut();
 
-            LocalDateTime currentDateTime = LocalDateTime.now();
-            DateTimeFormatter fm = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
-            String formattedDateTime = currentDateTime.format(fm);
 
             //write date, time & receipt details to file
             writer.write("Date and Time: " + formattedDateTime + "\n" );
-           // writer.write(receiptDetails);
+
+
+            writer.close();
+
 
             System.out.println("Receipt file saved successfully. ");
         }catch (IOException e) {
